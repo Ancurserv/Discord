c28690a013863e8e55fd4bf60113fed01a033529
Ancurserv <67490632+Ancurserv@users.noreply.github.com>
2021-02-22 12:28:47 -0500

    updated

--- AlarmClock/Alarm.py
+++ AlarmClock/Alarm.py
@@ -1,9 +1,11 @@
 import datetime
 import time
-# Alarm Clock "Bot" Example - This is simply just a unit code test for an alarm clock functionality so all you need to do is setup a Discord Bot and implement this code within it but of course replace the input function with the discord.py function that would be use to extract a message you should also insert it within a while loop or else it will only extract 1 message and end. 
+"""
+Alarm Clock "Bot" Example - This is simply just a unit code test for an alarm clock functionality so all you need to do is setup a Discord Bot and implement this code within it but of course replace the input function with the discord.py function that would be use to extract a message you should also insert it within a while loop or else it will only extract 1 message and end.
 
-# The usage of the time and datetime module here will only work if you are running the bot from your device and not from a cloud server because by default the device is registered to your area timezone however
-#for a cloud server you would need to manually set a specific time zone
+The usage of the time and datetime module here will only work if you are running the bot from your device and not from a cloud server because by default the device is registered to your area timezone however
+for a cloud server you would need to manually set a specific time zone
+"""
 x = input("Insert command: ")
 y = x.split()
 if y[0] == "!remind":
